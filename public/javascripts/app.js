4b620aac40672074b1c7243278734cb1d5f977eb
Philip Seger <seger.philip@gmail.com>
2015-03-24 15:59:47 -0400

    fitting canvas without scrollbars

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,12 @@
 function init(){
 	canvas = document.getElementById("blankSpace");
-	canvas.width = $(window).width();
-	canvas.height = $(window).height();
+	canvas.width = $(window).width()-20;
+	canvas.height = $(window).height()-20;
 	var ctx = canvas.getContext("2d");
 	ctx.beginPath();
 	ctx.arc(95,50,40,0,2*Math.PI);
 	ctx.stroke();
+  var rec = canvas.getContext("2d");
+  rec.fillStyle = "#FF0000";
+  rec.fillRect(250,250,250,175);
 }
