27de6eb1d84289959ea8e9cb75bfbbaae004ede7
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-03-24 16:49:13 -0400

    animated circles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,33 @@
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+var radius = 10;
+
 function init(){
 	canvas = document.getElementById("blankSpace");
 	canvas.width = $(window).width()-20;
 	canvas.height = $(window).height()-20;
-	var ctx = canvas.getContext("2d");
+  	drawStuff();
+}
+
+function drawCircle(ctx){
 	ctx.beginPath();
-	ctx.arc(95,50,40,0,2*Math.PI);
+	ctx.arc(95,50,radius,0,2*Math.PI);
 	ctx.stroke();
-  var rec = canvas.getContext("2d");
-  rec.fillStyle = "#FF0000";
-  rec.fillRect(250,250,250,175);
+	ctx.closePath();
+	radius += 10;
+}
+
+function drawRectangle(ctx){
+	ctx.fillStyle = "#FF0000";
+  	ctx.fillRect(250,250,250,175);
+}
+
+function drawStuff(){
+	ctx = canvas.getContext("2d");
+
+	drawCircle(ctx);
+	drawRectangle(ctx);
+	requestAnimationFrame(drawStuff);
 }
