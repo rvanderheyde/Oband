0d622a8ed97982d1c39dbb00191af46ed28b89e5
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-03-28 10:58:27 -0400

    Start Screen UI made

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -3,8 +3,8 @@ var requestAnimationFrame = window.requestAnimationFrame ||
                             window.webkitRequestAnimationFrame || 
                             window.msRequestAnimationFrame;
 
-var height = $(window).height()-20;
-var width = $(window).width()-20;
+var height = $(window).height()-5;
+var width = $(window).width()-5;
 var socket = io();
 
 function init(){
@@ -18,7 +18,20 @@ function init(){
 }
 
 function drawStartScreen(){
-	
+	ctx.fillStyle = "#909090";
+  	ctx.fillRect(0,0,width,height);
+  	ctx.fillStyle = "#656565"
+  	ctx.fillRect(width/2-90,270,180, 45)
+  	ctx.fillRect(width/2-90, 320,180, 45)
+  	ctx.fillRect(width/2-90,370,180, 45)
+  	ctx.fillStyle = "#000000"
+  	ctx.font = "Bold 128px Arial"
+  	ctx.fillText('Oband', width/2-200, 228)
+  	ctx.font = "Bold 24px Arial"
+  	ctx.fillText('Single Player', width/2-75, 300)
+  	ctx.fillText('Multi-Player', width/2-67, 350)
+  	ctx.fillText('Help', width/2-24, 400)
+  	
 }
 
 function mouseClickDown(event){
