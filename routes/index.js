8d6075980425513ff39dd3cd2dc41d40c90852ed
Philip Seger <seger.philip@gmail.com>
2015-03-28 17:00:17 -0400

    echonest api setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var path = require('path');
+var echojs = require('echojs');
+
 routes = {}
 
-routes.indexRender = function(req, res){
+routes.indexRender = function (req, res){
   var url = path.resolve( __dirname + '../../views/index.html');
   res.sendFile(url);
-};
+}
+
+var echo = echojs({
+  key: process.env.ECHONEST_KEY
+});
+
+// testing if api works
+routes.testing = function (req, res) {
+  echo('song/search').get({
+    artist: 'abba',
+    title: 'mamma mia'
+  }, function (err, json) {
+    console.log(json.response);
+  });
+}
 
 module.exports = routes
