4da9543a5a0ca9483af432c74fbda37c471524be
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-04-14 14:12:48 -0400

    Commenting code and changing spacing

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,3 +1,4 @@
+//Global stuff
 var requestAnimationFrame = window.requestAnimationFrame || 
                             window.mozRequestAnimationFrame || 
                             window.webkitRequestAnimationFrame || 
@@ -33,123 +34,131 @@ var score = 0;
 var input = {a: false, s: false, d: false, f: false, g:false}
 
 function timestamp() {
+  //find how time delay is between frame updates
   return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
 }
 
 function songFinished(songObj){
-	if(songObj.song.length !== 0){
-		return false;
-	} else {
-		return true;
-	}
+  //check if song is over
+  if(songObj.song.length !== 0){
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function onKey(ev, key, pressed){
-	switch(key){
-		case KEY.A: input.a = pressed; break;
-		case KEY.S: input.s = pressed; break;
-		case KEY.D: input.d = pressed; break;
-		case KEY.F: input.f = pressed; break;
-		case KEY.G: input.g = pressed; break;
-	}
+  //set input based on key response
+  switch(key){
+    case KEY.A: input.a = pressed; break;
+    case KEY.S: input.s = pressed; break;
+    case KEY.D: input.d = pressed; break;
+    case KEY.F: input.f = pressed; break;
+    case KEY.G: input.g = pressed; break;
+  }
 }
 
 function update(dt){
-	var song = Global.song.song;
-	for(var i = 0; i<song.length; i++){
-		if(song[i].time < 100){
-			var note = song.shift()
-			for(var i=0; i<note.keys.length; i++){
-				console.log(input)
-				if (note.keys[i] === 'A' && input.a){
-					score += 10;
-					alert('SCORE 1')
-				} else if (note.keys[i] === 'S' && input.s){
-					score += 10;
-					alert('SCORE 2')
-				} else if (note.keys[i] === 'D' && input.d){
-					score += 10;
-				} else if (note.keys[i] === 'F' && input.f){
-					score += 10;
-				} else if (note.keys[i] === 'G' && input.g){
-					score += 10;
-				} else {
-					if (input.a || input.s || input.d || input.f || input.g){
-						score -= 5;
-					} else {
-						score -= 2;
-					}
-				}
-			}
-			//Update scores on all cleints via sockets here. 
-		} else {
-			song[i].time -= dt;
-		}
-	}
-	Global.song.song = song;
+  //change the game state based on time, User input
+  var song = Global.song.song;
+  for(var i = 0; i<song.length; i++){
+    if(song[i].time < 100){
+      var note = song.shift()
+      for(var i=0; i<note.keys.length; i++){
+        console.log(input)
+        if (note.keys[i] === 'A' && input.a){
+          score += 10;
+          alert('SCORE 1')
+        } else if (note.keys[i] === 'S' && input.s){
+          score += 10;
+          alert('SCORE 2')
+        } else if (note.keys[i] === 'D' && input.d){
+          score += 10;
+        } else if (note.keys[i] === 'F' && input.f){
+          score += 10;
+        } else if (note.keys[i] === 'G' && input.g){
+          score += 10;
+        } else {
+          if (input.a || input.s || input.d || input.f || input.g){
+            score -= 5;
+          } else {
+            score -= 2;
+          }
+        }
+      }
+      //Update scores on all cleints via sockets here. 
+    } else {
+      song[i].time -= dt;
+    }
+  }
+  Global.song.song = song;
 }
 var loop = 0;
 
 function render(){
-	canvas.setBackgroundColor('#999999');
-	canvas.createBackground()
-	canvas.setPenColor('#656565')
-	canvas.drawRect(canvas.width*.125, canvas.height*.05, canvas.width*.5, canvas.height*.9)
-	canvas.setPenColor('#000000')
-	canvas.drawLine(canvas.width*.125, canvas.height*.8, canvas.width*.625, canvas.height*.8)
-	canvas.setPenColor('#440044')
-	for(var i=0; i<Global.song.song.length; i++){
-		var note = Global.song.song[i]
-		for(var j=0; j<note.keys.length; j++){
-			if (note.keys[j] === 'A'){
-				var dx = timeToX(note.time)
-				canvas.drawRect(canvas.width*.225,(5000-note.time)/6250*canvas.height ,50,50) 
-			}
-			if (note.keys[j] === 'S'){
-				var dx = timeToX(note.time)
-				canvas.drawRect(canvas.width*.325,(5000-note.time)/6250*canvas.height ,50,50) 
-			}
-		}
-	}
-	var str = loop.toString()
-	canvas.drawText(str, canvas.width*.75, canvas.height*.5)
-	loop += 1;
+  //draws the game state on screen
+  canvas.setBackgroundColor('#999999');
+  canvas.createBackground()
+  canvas.setPenColor('#656565')
+  canvas.drawRect(canvas.width*.125, canvas.height*.05, canvas.width*.5, canvas.height*.9)
+  canvas.setPenColor('#000000')
+  canvas.drawLine(canvas.width*.125, canvas.height*.8, canvas.width*.625, canvas.height*.8)
+  canvas.setPenColor('#440044')
+  for(var i=0; i<Global.song.song.length; i++){
+    var note = Global.song.song[i]
+    for(var j=0; j<note.keys.length; j++){
+      if (note.keys[j] === 'A'){
+        var dx = timeToX(note.time)
+        canvas.drawRect(canvas.width*.225,(5000-note.time)/6250*canvas.height ,50,50) 
+      }
+      if (note.keys[j] === 'S'){
+        var dx = timeToX(note.time)
+        canvas.drawRect(canvas.width*.325,(5000-note.time)/6250*canvas.height ,50,50) 
+      }
+    }
+  }
+  var str = loop.toString()
+  canvas.drawText(str, canvas.width*.75, canvas.height*.5)
+  loop += 1;
 
 }
 
 function timeToX(time){
-	//turn time on range 0-5000 to x. 
-	//5000 = 0
-	//0 = canvas.height*.8
-	return (5000-time)/4000*canvas.height 
+  //turn time on range 0-5000 to x. 
+  //5000 = 0
+  //0 = canvas.height*.8
+  return (5000-time)/6250*canvas.height 
 }
 
 
 function mainGame(songObj){
-	if(!songFinished(songObj)){
-		now = timestamp();
-		dt += Math.min(1000, (now - last));
-		while(dt > step) {
-    		dt -= step;
-    		update(step);
-    	}
-		render(dt)
-		last = now;
-		requestAnimationFrame(function(){ mainGame(songObj) })
-	}
+  //game loop function
+  if(!songFinished(songObj)){
+    now = timestamp();
+    dt += Math.min(1000, (now - last));
+    while(dt > step) {
+        dt -= step;
+        update(step);
+      }
+    render(dt)
+    last = now;
+    requestAnimationFrame(function(){ mainGame(songObj) })
+  }
 }
 
 function playGame(songObj){
-	canvas = gf.fullCanvas();
-	Global.song = songObj;
-	document.addEventListener('keydown', function(ev){ onKey(ev, ev.keyCode, true), false})
-	document.addEventListener('keyup', function(ev){ onKey(ev, ev.keyCode, false), false})
-	requestAnimationFrame(function(){ mainGame(songObj) })
+  //function that starts the game
+  canvas = gf.fullCanvas();
+  Global.song = songObj;
+  document.addEventListener('keydown', function(ev){ onKey(ev, ev.keyCode, true), false})
+  document.addEventListener('keyup', function(ev){ onKey(ev, ev.keyCode, false), false})
+  requestAnimationFrame(function(){ mainGame(songObj) })
 }
 
 function main(){
-	var songObj = { song: [{time: 5000, keys:['A']},{time: 10000, keys:['A','S']},]}
-	playGame(songObj)
+  //test function
+  var songObj = { song: [{time: 5000, keys:['A']},{time: 10000, keys:['A','S']},]}
+  playGame(songObj)
 }
 
 
