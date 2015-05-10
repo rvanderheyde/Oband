fd5a3012ce6de368d795fd3ff23451dc5f7d173b
Rvanderheyde <radmer.vanderheyde@students.olin.edu>
2015-05-10 19:18:52 -0400

    OOPS

--- oauth.js
+++
@@ -1,9 +0,0 @@
-var exports = {};
-
-exports.facebook = {
-  clientID: '1403337959989971',
-  clientSecret: '5a56459669ac50fb3d85740392b0cb6e',
-  callbackURL: 'http://localhost:3000/auth/facebook/callback'
-};
-
-module.exports = exports;
